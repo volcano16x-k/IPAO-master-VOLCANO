695655ef16d92b08b5776efe41b01b029d143786
volcano16x-k <volcanox777x@gmail.com>
2026-09-25 13:44:09 -0700

    Update LicenseManager.swift

--- ThreeOneOSFive/helpers/LicenseManager.swift
+++ ThreeOneOSFive/helpers/LicenseManager.swift
@@ -4,7 +4,7 @@ import Security
 
 @MainActor
 final class LicenseManager: ObservableObject {
-    static let accessKey = "VOLCANO"
+    static let accessKey = "NovoX7"
 
     @Published private(set) var expirationDate: Date?
     @Published private(set) var isActive = false
