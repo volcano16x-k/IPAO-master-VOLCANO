75f0c7a90f38336db0a07054e33a693bd563a1ea
volcano16x-k <volcanox777x@gmail.com>
2026-09-24 21:58:37 -0700

    Update ContentView.swift

--- ThreeOneOSFive/ContentView.swift
+++ ThreeOneOSFive/ContentView.swift
@@ -114,7 +114,7 @@ struct ContentView: View {
                 patchCard(name: "144 FPS", target: "FREE FIRE • NORMAL", package: "VOLCANO File (10).3105", color: AppTheme.secondaryAccent, state: $hyperBalamagicaEnabled)
                 patchCard(name: "Aim Body", target: "FREE FIRE • NORMAL", package: "VOLCANO File (12).3105", color: AppTheme.accent, state: $aimBodyPackageEnabled)
                 patchCard(name: "Aim Chest", target: "FREE FIRE • NORMAL", package: "VOLCANO File (2).3105", color: AppTheme.secondaryAccent, state: $aimChestPackageEnabled)
-                patchCard(name: "Magic", target: "FREE FIRE • NORMAL", package: "VOLCANO File (14).3105", color: AppTheme.accent, state: $magicEnabled)
+                patchCard(name: "panel Flash", target: "FREE FIRE • NORMAL", package: "VOLCANO File (14).3105", color: AppTheme.accent, state: $magicEnabled)
             }
 
             HStack(spacing: 8) {
